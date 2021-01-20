9c6c0f8c6d3e6b0a6340c7d8d87184bbd733dac2
Frances-Janelle-Torres <frances.janelle.torres@gmail.com>
2021-01-20 13:04:22 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Weather from "./Weather"
 
+
 function App() {
   return (
     <div className="App">
       <h1> Weather Search Engine </h1>
+      <hr />
       <Weather />
     </div>
   );
---
+++ src/footer.js
@@ -0,0 +1,8 @@
+import react from "react"
+
+export default function Foot() {
+    return (
+        <p>
+          GitHub Repository by Frances-Janelle Torres   </p>
+    )
+}

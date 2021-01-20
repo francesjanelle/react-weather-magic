c7b546f767676da8c97b504d647d2a27fe34006e
Frances-Janelle-Torres <frances.janelle.torres@gmail.com>
2021-01-20 12:54:41 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Weather from "./Weather"
 
 function App() {
   return (
     <div className="App">
       <h1> Weather Search Engine </h1>
-  
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,40 @@
 import React, { useState } from "react"
-import axios from "axious";
+ import axios from "axios";
 import "./index.css"
 
 export default function Weather(props) {
     const [city, setCity] = useState("");
-    const [text, setMessage] = useState("");
+    const [text, setText] = useState("");
+
+    function showTemp(response) {
+        let temp = Math.round(response.data.main.temp);
+        let fahr = Math.round(Math.round(response.data.main.temp) * (9 / 5) + 32)
+        //seriously, there has to be a better way doing this...
+
+        setText(
+            <div className="message">
+                The temperature is {temp}°C ({fahr})°F in {city}
+            </div>
+        )
+    }
 
     function handleSearch(event) {
         event.preventDefault();
-
         let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=deb4d0036edfa966c7a36750fd024ceb&units=metric`;
-    axios.get(apiUrl).then(showTemp);
+        axios.get(apiUrl).then(showTemp);
+    }
+
+    function updateSearch(event) {
+        setCity(event.target.value)
     }
 
     return (
         <div className="searchbox">
             <form onSubmit={handleSearch}> 
-
-            </form>
+                <input type="search" placeholder="Location" onChange={updateSearch} />
+                <input type="submit" value="Search" /> {" "} 
+            </form> {" "}
+            <h4> {text} </h4>
         </div>
     )
 }
